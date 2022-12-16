f784877c11cb46d3cac53cf57a3ff26448648b19
waltercervi <walter.rossicervi@wur.nl>
2022-12-16 10:23:12 +0100

    Initial commit

--- R/CreateBaseline.R
+++ R/CreateBaseline.R
@@ -8,15 +8,7 @@ doCreateBaseline<-function(){
   for (s in 1:length(SectorsNm)){
     print(paste("Baseline -",SectorsNm[s]))
     #s=2
-    fileNm=paste("MagnetGridR/SpatialData/ThematicMaps/SectorLandUseMaps/",Scenarios,"/",SectorsNm[s],"Area",sep="") #!! World
-    LUmap=ReadRdaTab(fileNm)
-    LUmap=subset(LUmap,LUmap$ISO3==Scenarios)[,c(1,2,4)]
-    coordinates(LUmap) <- ~ X + Y
-    gridded(LUmap) <- TRUE
-    LUmap <- raster(LUmap)
-    projection(LUmap)=CRS.WGS84
-    #Shift is required to combine rasters
-    LUmap=doReprojShift(LUmap,TRUE)
+    LUmap <- raster(paste("MagnetGridR/SpatialData/InputData_LandUseModel/",Scenarios,"/SectorLandUse/",SectorsNm[s],".tif",sep=""))
     writeRaster(LUmap,paste("MagnetGridR/SpatialData/InputData_LandUseModel/",Scenarios,"/2014/LandUse/",SectorsNm[s],".tif",sep=""), overwrite=T)
   }
 }
